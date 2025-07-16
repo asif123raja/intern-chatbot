5d5e8728b19015d061c8b185d402bbc5cb46d0eb
Md Asiful Ameen <asifulameen208@gmail.com>
2025-07-16 20:09:05 +0530

    2 changes

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -18,8 +18,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/Frontend/'), // Update this line
   routes
 })
-
 export default router
+
