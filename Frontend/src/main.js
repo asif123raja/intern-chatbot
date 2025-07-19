4a84b1479be32f121ac27a65a9e63cf50ee944c5
Mihir-patil-2242 <mihirpatil2242@gmail.com>
2025-07-19 16:28:17 +0530

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,13 +5,13 @@ import './index.css'
 
 createApp(App).use(router).mount('#app')
 
+
 async function sendMessage(message) {
-  const response = await fetch("http://127.0.0.1:8000/chat", {
+  const res = await fetch("http://127.0.0.1:8000/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message }),
   });
-
-  const data = await response.json();
-  return data.reply;
+  const data = await res.json();
+  return data.response;
 }
